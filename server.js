e03fb3f79cd9bb758ea6df87614e46a7181965f2
Ian Febi S <ianfebi01@gmail.com>
2022-09-17 22:37:16 +0700

    change port

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 app.use(cors());
 app.use(
   fileUpload({
