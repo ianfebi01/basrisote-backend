a32632c511c2b101fdc56c260e875382a091daaa
Ian Febi S <ianfebi01@gmail.com>
2022-09-24 15:09:29 +0700

    add edit post

--- controllers/post.js
+++ controllers/post.js
@@ -19,6 +19,27 @@ exports.getAllPosts = async (req, res) => {
     return res.status(500).json({ message: error.message });
   }
 };
+exports.updatePost = async (req, res) => {
+  try {
+    const { type, category, header, images, body, user } = req.body;
+
+    const postId = req.params.id;
+    const posts = await Post.findOneAndReplace(
+      { _id: postId },
+      {
+        type: type,
+        category: category,
+        header: header,
+        images: images,
+        body: body,
+        user: user,
+      }
+    );
+    res.json(posts);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
 
 exports.deletePost = async (req, res) => {
   try {
--- routes/post.js
+++ routes/post.js
@@ -1,11 +1,17 @@
 const express = require("express");
-const { createPost, getAllPosts, deletePost } = require("../controllers/post");
+const {
+  createPost,
+  getAllPosts,
+  deletePost,
+  updatePost,
+} = require("../controllers/post");
 const { authUser } = require("../middlewares/auth");
 
 const router = express.Router();
 
 router.post("/createPost", authUser, createPost);
 router.get("/getAllPosts", getAllPosts);
+router.patch("/updatePost/:id", authUser, updatePost);
 router.delete("/deletePost/:id", authUser, deletePost);
 
 module.exports = router;
